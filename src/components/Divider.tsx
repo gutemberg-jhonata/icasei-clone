b272b40894a393adbc2661a6b8eb1e73bddaa799
Gutemberg <gutemberg.jhonata@gmail.com>
2022-10-31 21:37:49 -0300

    Adding message

---
+++ src/components/Divider.tsx
@@ -0,0 +1,7 @@
+export function Divider() {
+    return (
+        <div className="divider">
+            <svg width="56" height="2" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="56" height="2" rx="1" fill="#C5AE82"></rect></svg>
+        </div>
+    )
+}
